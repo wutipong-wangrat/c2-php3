c197148010c03ad275f290c719410fe9bd5535b0
Ddvrg <bigbosszaa086@gmail.com>
2021-11-19 10:38:19 +0700

    initial commit

--- php301.php
+++ php301.php
@@ -1,3 +1,11 @@
 <?php
-    echo "Welcome to My site";
-?>
+    $x = 10;
+    $y = 5;
+    echo "<h1>PHP Arithmetic Operators</h1>";
+    echo "<h2>x = $x and y = $y", "<hr color ='red' size='4' /></h2>";
+    echo "$x + $y = ", $x + $y, "<br />";
+    echo "$x - $y = ", $x - $y, "<br />";
+    echo "$x * $y = ", $x * $y, "<br />";
+    echo "$x / $y = ", $x / $y, "<br />";
+    echo "$x % $y = ", $x % $y, "<br />";
+    echo "$x ^ $y = ", $x ** $y, "<br />";
--- php302.php
+++ php302.php
@@ -1,2 +1,12 @@
 <?php
-echo "Operator";
+$x = 120;
+
+echo "<h1>PHP Assignment Operators</h1>";
+echo "<h1> x = $x <br /></h1>";
+echo "<hr color='red' size='4px' />";
+
+echo "x += : ",$x += 10,"<br />";//$x = $x + 10
+echo "x -= : ",$x -= 10,"<br />";//$x = $x - 10
+echo "x *= : ",$x *= 10,"<br />";//$x = $x * 10
+echo "x /= : ",$x /= 10,"<br />";//$x = $x / 10
+echo "x %= : ",$x %= 10,"<br />";//$x = $x % 10
